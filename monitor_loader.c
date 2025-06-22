792fb71adf6a765a6392e6acaaf8cc31546bd037
shkch <cba7215@gmail.com>
2025-06-23 01:33:17 +0900

    로더.c 주석작성

--- monitor_loader.c
+++ monitor_loader.c
@@ -14,33 +14,33 @@
 #include "setns_monitor.skel.h"
 #include "symlinkat_monitor.skel.h"
 
-static rd_kafka_t *rk;
+static rd_kafka_t *rk; //카프카 프로듀서 핸들
 
-void init_kafka() {
-    char err[512];
-    rd_kafka_conf_t *conf = rd_kafka_conf_new();
-    rd_kafka_conf_set(conf, "bootstrap.servers", "localhost:9092", err, sizeof(err));
-    rk = rd_kafka_new(RD_KAFKA_PRODUCER, conf, err, sizeof(err));
+void init_kafka() { //카프카 초기화 함수
+    char err[512]; 
+    rd_kafka_conf_t *conf = rd_kafka_conf_new(); //카프카 설정객체 생성
+    rd_kafka_conf_set(conf, "bootstrap.servers", "localhost:9092", err, sizeof(err)); //카프카 서버주소 설정
+    rk = rd_kafka_new(RD_KAFKA_PRODUCER, conf, err, sizeof(err)); 카프카 프로듀서 생성
 }
 
 static int on_event(void *ctx, void *data, size_t size) {
-    struct event_t *e = data;
-    char buf[256];
+    struct event_t *e = data; //커널에서 넘어온 raw데이터 구조체로 캐스팅
+    char buf[256]; //카프카로 보낼 JSON 메세지 버퍼
     int len = snprintf(buf, sizeof(buf),
         "{\"type\":%u,\"pid\":%u,\"ts\":%llu}\n",
         e->type, e->pid, e->ts_ns);
     rd_kafka_producev(
         rk,
-        RD_KAFKA_V_TOPIC("syscall_events"),
-        RD_KAFKA_V_MSGFLAGS(RD_KAFKA_MSG_F_COPY),
-        RD_KAFKA_V_VALUE(buf, len),
-        RD_KAFKA_V_END
+        RD_KAFKA_V_TOPIC("syscall_events"), //토픽 이름 : syscall_events 
+        RD_KAFKA_V_MSGFLAGS(RD_KAFKA_MSG_F_COPY), //버퍼 복사하여 보관
+        RD_KAFKA_V_VALUE(buf, len), //보낼 실제 데이터(메시지 페이로드)
+        RD_KAFKA_V_END //가변인자 전달 종료 표시
     );
     return 0;
 }
 
 int main() {
-    struct ring_buffer *rb;
+    struct ring_buffer *rb; //eBPF 링버퍼 핸들
     int map_fd;
 
     // 각 BPF skeleton open/load/attach
@@ -73,11 +73,11 @@ int main() {
 
     // ring buffer는 공통 map 사용한다고 가정
     // 여기서는 clone_monitor의 이벤트 map을 예로 사용
-    map_fd = bpf_map__fd(clone_skel->maps.events);
-    rb = ring_buffer__new(map_fd, on_event, NULL, NULL);
+    map_fd = bpf_map__fd(clone_skel->maps.events); //맵파일디스크립터
+    rb = ring_buffer__new(map_fd, on_event, NULL, NULL); //링버퍼 핸들생성
     if (!rb) return 1;
 
-    init_kafka();
+    init_kafka(); //카프카 초기화
     printf("Monitoring → Kafka(syscall_events)... Ctrl+C to exit\n");
     while (1)
         ring_buffer__poll(rb, 100);
